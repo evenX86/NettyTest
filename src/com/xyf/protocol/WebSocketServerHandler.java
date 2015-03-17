82abe12a543c9eb836b5383f7e44b870fb87f7ae
evenX86 <evenX86@gmail.com>
2015-03-17 14:08:51 +0800

    [add]增加websocket服务端处理类，以及测试页面

--- src/com/xyf/protocol/WebSocketServerHandler.java
+++ src/com/xyf/protocol/WebSocketServerHandler.java
@@ -1,14 +1,92 @@
 package com.xyf.protocol;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.logging.Logger;
 
 /**
  * Created by xuyifei01 on 2015/3/16.
  */
 public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+    private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+    private WebSocketServerHandshaker handshaker;
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //传统的HTTP接入
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx,(FullHttpRequest)msg);
 
+        } else if (msg instanceof WebSocketFrame){ //websocket接入
+            handleWebSocketFrame(ctx,(WebSocketFrame)msg);
+        }
+    }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.flush();
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        //是否关闭链路
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        //是否是ping信息
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        //仅支持文本信息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException("仅支持文本信息");
+        }
+        String request = ((TextWebSocketFrame) frame).text();
+        ctx.channel().write(new TextWebSocketFrame(request + "欢迎使用Netty Socket服务"));
+    }
+
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        //http解码失败则返回http异常
+        if (!req.decoderResult().isSuccess()||(!"websocket".equals(req.headers().get("Upgrade")))) {
+            sendHttpResponse(ctx,req,new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        //构造握手响应返回，本机测试
+        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory("ws://localhost:8080/websocket",null,false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null){
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(),req);
+        }
+    }
+
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
+        if(res.status().code()!=200){
+            ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+        //    res.headers().setContentLength(res,res.content().readableBytes());
+            ChannelFuture f = ctx.channel().writeAndFlush(res);
+            if (res.status().code()!=200) {
+                f.addListener(ChannelFutureListener.CLOSE);
+            }
+        }
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
